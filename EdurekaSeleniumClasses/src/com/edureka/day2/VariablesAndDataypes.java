a59765cc34b6e35fdad7525fb8be52e6c7c63c3c
sudhir <sudhir@app2mobile.com>
2019-04-05 13:33:37 -0400

    Day 2:Summary FunctionTypes and Constructors

---
+++ EdurekaSeleniumClasses/src/com/edureka/day2/VariablesAndDataypes.java
@@ -0,0 +1,30 @@
+package com.edureka.day2;
+
+public class VariablesAndDataypes {
+
+	public static void main(String[] args) {
+		byte mybytevar=10;          // 1 byte
+		short myshortvar=100;       //2 byte
+		int myintvar=12000;			//4 byte
+		long mylongvar=89624l;		//8 byte
+		float myfloatvar=3.236542f; //4 byte
+		double mydoublevar=78.693258741; //	8byte
+		boolean mybooleanvar=true;		//1 byte
+		char mycharvar='7';             //1 byte
+		System.out.println("byte val:"+mybytevar);
+		System.out.println("short val:"+myshortvar);
+		System.out.println("int val:"+myintvar);
+		System.out.println("long val:"+mylongvar);
+		System.out.println("float val:"+myfloatvar);
+		System.out.println("double val:"+mydoublevar);
+		System.out.println("boolean val:"+mybooleanvar);
+		System.out.println("char val:"+mycharvar);
+		
+		
+		
+
+	}
+
+	
+
+}
