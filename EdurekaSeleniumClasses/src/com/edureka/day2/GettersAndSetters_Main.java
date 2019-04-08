cdf9f4b7c1bc66f2c0a222d2134c27ea4ea58455
C O Mitters <committer@example.com>
2019-04-08 03:48:41 -0400

    Day2:getters and setters

---
+++ EdurekaSeleniumClasses/src/com/edureka/day2/GettersAndSetters_Main.java
@@ -0,0 +1,12 @@
+package com.edureka.day2;
+
+public class GettersAndSetters_Main {
+
+	public static void main(String[] args) {
+		GettersAndSetters kanika=new GettersAndSetters();
+		kanika.setsalary(50000);
+		kanika.setBonus(5000);
+		kanika.calculatesalary();
+	}
+
+}
